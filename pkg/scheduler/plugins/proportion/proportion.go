8eb505e833d3984156160756e302997ece71db5f
zhangyunyao <aidaizyy@gmail.com>
2021-11-29 16:58:42 +0800

    fix queue allocated exceeds capability
    Signed-off-by: zhangyunyao <aidaizyy@gmail.com>

--- pkg/scheduler/plugins/proportion/proportion.go
+++ pkg/scheduler/plugins/proportion/proportion.go
@@ -20,7 +20,6 @@ import (
 	"math"
 	"reflect"
 
-	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog"
 
 	"volcano.sh/apis/pkg/apis/scheduling"
@@ -257,24 +256,18 @@ func (pp *proportionPlugin) OnSessionOpen(ssn *framework.Session) {
 		return overused
 	})
 
-	ssn.AddUnderusedResourceFn(pp.Name(), func(queue *api.QueueInfo) api.ResourceNameList {
-		underUsedResNames := api.ResourceNameList{}
+	ssn.AddAllocatableFn(pp.Name(), func(queue *api.QueueInfo, candidate *api.TaskInfo) bool {
 		attr := pp.queueOpts[queue.UID]
 
-		_, underUsedResource := attr.allocated.Diff(attr.deserved, api.Zero)
-		if underUsedResource.MilliCPU >= api.GetMinResource() {
-			underUsedResNames = append(underUsedResNames, v1.ResourceCPU)
+		toAllocated := attr.allocated.Clone()
+		toAllocated.Add(candidate.Resreq)
+		allocatable := toAllocated.LessEqual(attr.deserved, api.Zero)
+		if !allocatable {
+			klog.V(3).Infof("Queue <%v>: deserved <%v>, allocated <%v>; Candidate <%v>: resource request <%v>",
+				queue.Name, attr.deserved, attr.allocated, candidate.Name, candidate.Resreq)
 		}
-		if underUsedResource.Memory >= api.GetMinResource() {
-			underUsedResNames = append(underUsedResNames, v1.ResourceMemory)
-		}
-		for rName := range underUsedResource.ScalarResources {
-			underUsedResNames = append(underUsedResNames, rName)
-		}
-		klog.V(3).Infof("Queue <%v>: deserved <%v>, allocated <%v>, share <%v>, underUsedResName %v",
-			queue.Name, attr.deserved, attr.allocated, attr.share, underUsedResNames)
 
-		return underUsedResNames
+		return allocatable
 	})
 
 	ssn.AddJobEnqueueableFn(pp.Name(), func(obj interface{}) int {
